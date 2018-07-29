e472efdf0c33f4efad89228785f2e0f79d373895
aiqiaodaima <754925121@qq.com>
2018-07-29 17:58:58 +0800

    2018/07/29

--- js/public.js
+++ js/public.js
@@ -1,7 +1,7 @@
 var Public = window.Public || {};
 Public.url = (()=>{
-    var url = 'http://192.168.1.150:1002';
-    // var url = 'http://hszj.yraise.com';
+    // var url = 'http://192.168.1.150:1002';
+    var url = 'http://hszj.yraise.com';
     return url
 })
 
