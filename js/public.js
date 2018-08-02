6e551aa078d5bb55f1632b1984f4a181a2d4e269
aiqiaodaima <754925121@qq.com>
2018-08-02 19:49:26 +0800

    2018/08/02

--- js/public.js
+++ js/public.js
@@ -2,6 +2,7 @@ var Public = window.Public || {};
 Public.url = (()=>{
     var url = 'http://192.168.1.150:1002';
     // var url = 'http://hszj.yraise.com';
+    // var url = 'http://www.redkross.com';
     return url
 })
 
