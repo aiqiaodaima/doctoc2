0caf53b5444db27e535e27d1015a25c6973fcdd7
aiqiaodaima <754925121@qq.com>
2018-08-05 03:00:03 +0800

    2018/08/05

--- js/public.js
+++ js/public.js
@@ -1,8 +1,8 @@
 var Public = window.Public || {};
 Public.url = (()=>{
-    var url = 'http://192.168.1.150:1002';
+    // var url = 'http://192.168.1.150:1002';
     // var url = 'http://hszj.yraise.com';
-    // var url = 'http://www.redkross.com';
+    var url = 'http://www.redkross.com';
     return url
 })
 
