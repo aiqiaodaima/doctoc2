aee3751bacd19885803352c7337e239b222dff36
aiqiaodaima <754925121@qq.com>
2018-08-08 23:53:45 +0800

    2018/08/08

--- js/public.js
+++ js/public.js
@@ -1,7 +1,7 @@
 var Public = window.Public || {};
 Public.url = (()=>{
-    //    var url = 'http://192.168.1.150:1002';
-        var url = 'http://192.168.1.178:1002'
+       var url = 'http://192.168.1.150:1002';
+        // var url = 'http://192.168.1.178:1002'
     // var url = 'http://www.redkross.com';
     return url
 })
